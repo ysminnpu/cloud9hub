399c96382cb16cd06cd9cc460fe4a1366f052493
Shoumeng Yan <shoumeng.yan@intel.com>
2014-10-04 16:11:01 +0800

    use cloud9 instead of c9 as the local cloud9 directory
    Change-Id: I781c2afaff0d9923c3cb47426adecd31b121d08f

--- controllers/workspaces.js
+++ controllers/workspaces.js
@@ -146,9 +146,9 @@ exports.destroy = function(req, res) {
 
    if(typeof req.app.get('runningWorkspaces')[req.user + '/' + workspaceName] === 'undefined'){
        getNextAvailablePort(function(nextFreePort){
-            console.log("Starting " + __dirname + '/../../c9/bin/cloud9.sh for workspace ' + workspaceName + " on port " + nextFreePort);
+            console.log("Starting " + __dirname + '/../../cloud9/bin/cloud9.sh for workspace ' + workspaceName + " on port " + nextFreePort);
        
-            var workspace = spawn(__dirname + '/../../c9/bin/cloud9.sh', ['-w', __dirname + '/../workspaces/' + req.user + '/' + workspaceName, '-l', '0.0.0.0', '-p', nextFreePort], {detached: true});
+            var workspace = spawn(__dirname + '/../../cloud9/bin/cloud9.sh', ['-w', __dirname + '/../workspaces/' + req.user + '/' + workspaceName, '-l', '0.0.0.0', '-p', nextFreePort], {detached: true});
             workspace.stderr.on('data', function (data) {
                 console.log('stdERR: ' + data);
             });
